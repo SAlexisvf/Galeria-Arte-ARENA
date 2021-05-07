83fc80e048f0d97548610520b37e7902b09610a0
Ricardo Vela <aricav96@gmail.com>
2021-05-07 11:20:16 -0500

    Add Montserrat font to project
    This is the font used in the official webpage, so we might as well

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,22 +1,31 @@
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
 import Card from "react-bootstrap/Card"
 import CardDeck from "react-bootstrap/CardDeck"
 import { doneImages } from '../utils/importAll'
 
 const Gallery = () => {
     return (
-        <CardDeck>
-          {doneImages.map((img, index) => (
-            <Card key={index}>
-                <Card.Img
-                src={img}
-                variant="top"/>
-                <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>Some Text</Card.Text>
-                </Card.Body>
-            </Card>
-          ))}
-        </CardDeck>
+        <Container>
+          <CardDeck>
+            <Row>
+              {doneImages.map((img, index) => (
+                  <Col md={4} key={index}>
+                    <Card md={4}>
+                      <Card.Img
+                        src={img}
+                        variant="top"/>
+                      <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>Some Text</Card.Text>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+              ))}
+            </Row>
+          </CardDeck>
+        </Container>
     )
 }
 
