49225a78062af4a9719071364915cc79ae82363b
Ricardo Vela <aricav96@gmail.com>
2021-05-11 17:54:14 -0500

    Revamp page header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,22 @@
-import Jumbotron from 'react-bootstrap/Jumbotron'
-import Container from 'react-bootstrap/Container'
-import Row from 'react-bootstrap/Row'
-import Col from 'react-bootstrap/Col'
 import Carousel from 'react-bootstrap/Carousel'
+import Image from 'react-bootstrap/Image'
 import { wipImages } from '../utils/importAll'
+import arenaLogo from '../images/arena.png'
+import '../Header.css'
 
 const Header = () => {
     return (
-        <Jumbotron className="Jumbotron">
-          <Container>
-            <Row>
-              <Col md={6}>
-
-                <Carousel fade controls={false} indicators={false}>
-                  {wipImages.map((img, index) => (
-                    <Carousel.Item key={index}>
-                      <div style={{backgroundImage: `url(${img})`}} id="header-carousel-item"/>
-                    </Carousel.Item>
-                  ))}
-                </Carousel>
-
-              </Col>
-
-              <Col md={6}> <h1>Galería de Arte ARENA</h1> </Col>
-
-            </Row>
-          </Container>
-        </Jumbotron>
+      <Carousel fade controls={false} indicators={false}>
+        {wipImages.map((img, index) => (
+          <Carousel.Item key={index}>
+            <div style={{backgroundImage: `url(${img})`}} className="header-carousel-item-image"/>
+            <Carousel.Caption>
+              <h1>Galería de Arte</h1>
+              <Image alt="ARENA logo" src={arenaLogo} style={{"width": "50%"}}/>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
+      </Carousel>
     );
 }
 
