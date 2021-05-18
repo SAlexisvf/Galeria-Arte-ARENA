fd66aa9d2810173169b635f585983fb4e5b5b285
Antonio <a01382469@itesm.mx>
2021-05-18 02:43:07 -0500

    Footer component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/Header'
 import Gallery from './components/Gallery'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Gallery/>
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,52 @@
+import React from 'react'; 
+import '../Footer.css'
+import {FaTwitter} from "react-icons/fa"; 
+import {FaFacebook} from "react-icons/fa"; 
+
+
+const Footer = () => <footer className="page-footer font-small blue pt-4">
+    <div className="container-fluid text-center text-md-left">
+        <div className="row">
+            <div className="col-md-6 mt-md-0 mt-3">
+                <h5 className="text-uppercase">ARENA</h5>
+                <p>Atención Integral del Autismo: Galería de Arte.</p>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="https://www.facebook.com/autismoarena" role="button"
+                        ><i class="fab FaTwitter"></i> <FaFacebook icon= {FaFacebook}/> </a>
+
+                <a class="btn btn-outline-light btn-floating m-1" href="https://twitter.com/autismoarena" role="button"
+                ><i class="fab FaTwitter"></i> <FaTwitter icon= {FaTwitter}/> </a>
+
+            </div>
+
+            <hr className="clearfix w-100 d-md-none pb-0"/>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+                <h5 className="text-uppercase">Estudiante:</h5>
+                <ul className="list-unstyled">
+                    <li><a>Estudiante 1</a></li>
+                    <li><a>Estudiante 2</a></li>
+                    <li><a>Estudiante 3</a></li>
+                    <li><a>Estudiante 4</a></li>
+                </ul>
+            </div>
+
+            <div className="col-md-3 mb-md-0 mb-3">
+                <h5 className="text-uppercase">Matricula:</h5>
+                <ul className="list-unstyled">
+                    <li><a>A00123456</a></li>
+                    <li><a>A00123456</a></li>
+                    <li><a>A00123456</a></li>
+                    <li><a>A00123456</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+
+    <div className="footer-copyright text-center py-3">© 2021 Copyright:
+        <a> Instituto Tecnológico y de Estudios Superiores de Monterrey </a>
+    </div>
+
+</footer>
+
+export default Footer
